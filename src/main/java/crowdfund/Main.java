9a71e7cece05913d11dbcdc3507188538e4e30a1
amangu1905 <aman.24scse1410127@galgotiasuniversity.ac.in>
2025-11-26 08:35:35 +0530

    hllo

--- src/main/java/crowdfund/Main.java
+++ src/main/java/crowdfund/Main.java
@@ -8,10 +8,17 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        // Load the login.fxml file
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/crowdfund/fxml/login.fxml"));
+        // Create a scene with the loaded FXML
         stage.setScene(new Scene(loader.load()));
+        // Set the title of the stage
         stage.setTitle("CrowdFunding System");
+        // Show the stage
         stage.show();
     }
-    public static void main(String[] args) { launch(); }
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        launch();
+    }
 }
