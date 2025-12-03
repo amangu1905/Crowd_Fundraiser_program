720ea92775a5b0e291e9606a44ba2c7490d9c311
amangu1905 <aman.24scse1410127@galgotiasuniversity.ac.in>
2025-12-03 08:45:30 +0530

    Update Contribution.java

--- src/main/java/crowdfund/model/Contribution.java
+++ src/main/java/crowdfund/model/Contribution.java
@@ -1,19 +1,27 @@
 package crowdfund.model;
 
+import java.time.LocalDateTime;
+
 public class Contribution {
     private int id;
     private int campaignId;
     private int userId;
     private double amount;
+    private LocalDateTime date;
 
     public Contribution() {}
 
     public Contribution(int id, int campaignId, int userId, double amount) {
-        this.id = id; this.campaignId = campaignId; this.userId = userId; this.amount = amount;
+        this.id = id;
+        this.campaignId = campaignId;
+        this.userId = userId;
+        this.amount = amount;
+        this.date = LocalDateTime.now();
     }
 
     public int getId() { return id; }
     public int getCampaignId() { return campaignId; }
     public int getUserId() { return userId; }
     public double getAmount() { return amount; }
+    public LocalDateTime getDate() { return date; }
 }
